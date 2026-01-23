3440d513b781ee6d1c7d4b5d90c84f026dd95b72
tab2can <can@pekgezer.com>
2026-01-23 19:16:40 +0300

    -_-

---
+++ appids/962130.lua
@@ -0,0 +1,16 @@
+-- Grounded
+-- AppID 962130 | Generated on 2026-01-23 14:28 UTC | steamuck.com
+-- Note: This game supports Steam Workshop content
+
+-- Main Application
+addappid(962130, 1, "bdf60fa35009adc3e3856488fcc2037d94099c1f30faab7006bcdd6c4fbdc353")
+
+-- Content Depots (2)
+addappid(962131, 1, "809ed5f98fa8a3d2c3636c33504157cf05fc683e1810d35e554d7976573a4424") -- Maine - Windows
+addappid(962134, 1, "d8b1b4a7d68cf73a4aade13a9d230c6f1b7b397273cfad6b1a5713c631170002") -- Grounded - Content
+
+-- Shared Depots (4)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- VC 2010 Redist (from 228980)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") -- VC 2012 Redist (from 228980)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (from 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (from 228980)
