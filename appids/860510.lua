8893b5ed09ce5d0c1a5014b0465924e523ea624d
tab2can <can@pekgezer.com>
2026-01-30 23:13:20 +0300

    -_-

---
+++ appids/860510.lua
@@ -0,0 +1,22 @@
+-- Little Nightmares II
+-- AppID 860510 | Generated on 2026-01-30 20:10 UTC | steamuck.com
+-- Note: This game supports Steam Workshop content
+
+-- Main Application
+addappid(860510, 1, "7c19e104b37e48eae6442039e13f902a29ca43da03639cb64ce7bf904f3386ac")
+
+-- Content Depots (1)
+addappid(860511, 1, "b4785c13c7b51e8a08e3f16332ef4839c28d6e7544230b06535bd5ee180cbfea") -- Helios Content
+
+-- Shared Depots (3)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (from 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (from 228980)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") -- .NET 4.6 Redist (from 228980)
+
+-- DLCs with Content (2)
+addappid(1282531, 1, "1bf86c1151867753629f54ffb559c02482ff71f34f59476a0a058093e0635ddd") -- Little Nightmares II Digital Content Bundle
+addappid(1413420, 1, "8c10dd752f9e566421902b5f300bfc5098b40beaf62ba20c7ab8d223c1423dbf") -- Little Nightmares II Enhanced Edition
+
+-- DLCs without Dedicated Depots (2)
+addappid(1282530) -- Little Nightmares II Mokujin Hat
+addappid(1282532) -- Little Nightmares II The Nomes Attic
